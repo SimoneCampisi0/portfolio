b9ed78ef9c1e186f7004660ab150267c852c4939
SIMONE\simon <simonecampisi2003@gmail.com>
2023-10-19 17:06:59 +0200

    add transition in mobile menu

--- portfolio/src/app/app.module.ts
+++ portfolio/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderMenuComponent } from './header-menu/header-menu.component';
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { HeaderMenuComponent } from './header-menu/header-menu.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- portfolio/src/app/header-menu/header-menu.component.ts
+++ portfolio/src/app/header-menu/header-menu.component.ts
@@ -1,9 +1,26 @@
 import {Component, ElementRef, ViewChild} from '@angular/core';
+import {animate, style, transition, trigger} from "@angular/animations";
+
+const enterTransition = transition(':enter', [
+  style({
+    opacity: 0
+  }),
+  animate('0.5s ease-in', style({opacity: 1}))
+])
+const exitTransition = transition(':leave', [
+  style({
+    opacity: 1
+  }),
+  animate('0.5s ease-out', style({opacity: 0}))
+])
+const fadeIn = trigger('fadeIn', [enterTransition])
+const fadeOut = trigger('fadeOut', [exitTransition])
 
 @Component({
   selector: 'app-header-menu',
   templateUrl: './header-menu.component.html',
-  styleUrls: ['./header-menu.component.css']
+  styleUrls: ['./header-menu.component.css'],
+  animations: [fadeIn, fadeOut]
 })
 export class HeaderMenuComponent {
   svgData: string = '../../assets/list.svg'
