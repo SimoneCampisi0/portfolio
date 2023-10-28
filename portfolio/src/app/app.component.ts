247a20a06a0a5b3c45234e4bc0ed365085a860f4
SIMONE\simon <simonecampisi2003@gmail.com>
2023-10-28 19:32:13 +0200

    Vedi TODO.

--- portfolio/src/app/app.component.ts
+++ portfolio/src/app/app.component.ts
@@ -1,18 +1,48 @@
-import { Component } from '@angular/core';
-import {Router} from "@angular/router";
-import {RouteService} from "./service/routes.service";
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Router } from "@angular/router";
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'portfolio';
-  toggleChiSono: boolean = false;
+export class AppComponent implements AfterViewInit {
+  @ViewChild('homeSection') homeSection: ElementRef | undefined;
+  @ViewChild('chiSonoSection') chiSonoSection: ElementRef | undefined;
 
-  constructor(private routeService: RouteService) {
+  title = 'portfolio'
+  homeOffset: number = 0
+  chiSonoOffset: number = 0
+
+  constructor(private router: Router) { }
+
+  ngOnInit() {
+    window.addEventListener('scroll', () => {
+      this.onScroll();
+    });
   }
 
+  onScroll() { //TODO: sommare homeOffset + chiSonoOffset a document.documentElement.scrollTop per ottenere le altezze dei vari div in modo dinamico.
+    //  Far si che questo metodo sia utilizzabile nell'header.
+    // cambiaPagina(pagina: string) {
+    //     switch(pagina) {
+    //       case 'home':
+    //         document.documentElement.scrollTop = 0;
+    //         this.router.navigate(['/home']);
+    //         break;
+    //
+    //       case 'chi-sono':
+    //         document.documentElement.scrollTop = 800;
+    //         this.router.navigate(['/chi-sono']);
+    //         break;
+    //     }
+    //   }
+  }
 
+  ngAfterViewInit() {
+    if (this.homeSection && this.chiSonoSection) {
+      this.homeOffset = this.homeSection.nativeElement.offsetHeight; //Ottengo l'altezza del componente
+      this.chiSonoOffset = this.chiSonoSection.nativeElement.offsetHeight;
+    }
+  }
 }
--- portfolio/src/app/service/routes.service.ts
+++ portfolio/src/app/service/routes.service.ts
@@ -11,29 +11,38 @@ export class RouteService {
     });
   }
 
-  private onScroll(): void { //TODO: cambiare i pixel con valori relativi all'altezza della finestra in quel momento.
-    console.log(document.documentElement.scrollTop)
+  private onScroll() {
 
-    if(document.documentElement.scrollTop == 0 || document.documentElement.scrollTop < 600) {
-      this.router.navigate(['/home']);
-    }
-
-    if(document.documentElement.scrollTop > 600) {
-      this.router.navigate(['/chi-sono']);
-    }
   }
 
   cambiaPagina(pagina: string) {
-    switch(pagina) {
-      case 'home':
-        document.documentElement.scrollTop = 0;
-        this.router.navigate(['/home']);
-        break;
 
-      case 'chi-sono':
-        document.documentElement.scrollTop = 800;
-        this.router.navigate(['/chi-sono']);
-        break;
-    }
   }
+
+
+  // private onScroll(): void { //
+  //   console.log(document.documentElement.scrollTop)
+  //
+  //   if(document.documentElement.scrollTop == 0 || document.documentElement.scrollTop < 600) {
+  //     this.router.navigate(['/home']);
+  //   }
+  //
+  //   if(document.documentElement.scrollTop > 600) {
+  //     this.router.navigate(['/chi-sono']);
+  //   }
+  // }
+  //
+  // cambiaPagina(pagina: string) {
+  //   switch(pagina) {
+  //     case 'home':
+  //       document.documentElement.scrollTop = 0;
+  //       this.router.navigate(['/home']);
+  //       break;
+  //
+  //     case 'chi-sono':
+  //       document.documentElement.scrollTop = 800;
+  //       this.router.navigate(['/chi-sono']);
+  //       break;
+  //   }
+  // }
 }
