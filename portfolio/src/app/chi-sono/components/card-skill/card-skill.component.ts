74028e96732ff762042d24398ed2c873d10dc985
SIMONE\simon <simonecampisi2003@gmail.com>
2024-05-22 17:21:24 +0200

    Aggiunte cards competenze. Da rifinire lo stile.

--- portfolio/src/app/chi-sono/components/card-skill/card-skill.component.ts
+++ portfolio/src/app/chi-sono/components/card-skill/card-skill.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-card-skill',
   templateUrl: './card-skill.component.html',
   styleUrls: ['./card-skill.component.css']
 })
-export class CardSkillComponent {
+export class CardSkillComponent implements OnInit, OnDestroy {
+  @Input() nomeCard!: string;
+  @Input() icon!: string;
+  @Input() livelloCompetenza!: number; // range 1 - 5
+  livelloArr: number[] = [];
 
+  constructor() {
+  }
+
+  ngOnInit(): void {
+    for (let i = 1; i <= this.livelloCompetenza; i++) {
+      this.livelloArr.push(i);
+    }
+  }
+
+  ngOnDestroy(): void {
+  }
 }
