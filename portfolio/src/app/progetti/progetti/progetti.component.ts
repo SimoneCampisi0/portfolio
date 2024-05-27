349f5cfabd73d921dd4c7c07140c3df490fdc2de
SIMONE\simon <simonecampisi2003@gmail.com>
2024-05-27 12:00:25 +0200

    Rimosso bootstrap per preparare installazione MDB 5 per il carosello.

--- portfolio/src/app/progetti/progetti/progetti.component.ts
+++ portfolio/src/app/progetti/progetti/progetti.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import {AfterViewInit, Component, ElementRef, OnDestroy, OnInit, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-progetti',
   templateUrl: './progetti.component.html',
   styleUrls: ['./progetti.component.css']
 })
-export class ProgettiComponent {
+export class ProgettiComponent implements AfterViewInit, OnDestroy, OnInit {
+
+  slides = [
+    {img: "http://placehold.it/350x150/000000"},
+    {img: "http://placehold.it/350x150/111111"},
+    {img: "http://placehold.it/350x150/333333"},
+    {img: "http://placehold.it/350x150/666666"}
+  ];
+  slideConfig = {"slidesToShow": 4, "slidesToScroll": 4};
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+  }
+
+  ngAfterViewInit(): void {
+  }
+
+  ngOnDestroy(): void {
+  }
 
 }
