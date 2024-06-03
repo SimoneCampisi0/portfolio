e1771c983736e5baa8097eb9bd8fc15e7a59f608
SIMONE\simon <simonecampisi2003@gmail.com>
2024-06-03 17:55:35 +0200

    Form con invio mail funzionante.

--- portfolio/src/app/contattami/contattami.component.ts
+++ portfolio/src/app/contattami/contattami.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from "@angular/forms";
-import Swal from 'sweetalert2'
+import Swal from 'sweetalert2';
+import emailjs, {type EmailJSResponseStatus} from '@emailjs/browser';
+
 
 @Component({
   selector: 'app-contattami',
@@ -28,15 +30,34 @@ export class ContattamiComponent implements OnInit, OnDestroy {
   onSendMail() {
     console.log("values: ", this.formGroup)
 
-    if(this.formGroup.invalid) {
+    if (this.formGroup.invalid) {
       Swal.fire({
         icon: "error",
         title: "Dati non validi",
         text: "Inserisci correttamente i dati",
       });
+    } else {
+      this.sendEmailJS();
     }
 
-    
+  }
+
+  sendEmailJS() {
+    emailjs.init('DOUcryyfQzbERdPHu');
+
+    var templateParams = {
+      from_name: this.formGroup.controls['nomeCognome'].value,
+      mail_address: this.formGroup.controls['email'].value,
+      message: this.formGroup.controls['messaggio'].value
+    }
+
+    emailjs.send('service_ng4e4in', 'template_qbbjhdx', templateParams)
+      .then((response) => {
+          console.log("success: ", response.status, response.text)
+        },
+        (error) => {
+          console.log("failed: ", error)
+        })
   }
 
   ngOnDestroy(): void {
